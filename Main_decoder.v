6d2687bb08ae735cea2a2659fa04963c6e5b990b
Fahad <fahaddali301@gmail.com>
2026-08-26 19:25:35 +0500

    Update Main_decoder.v

--- Main_decoder.v
+++ Main_decoder.v
@@ -10,12 +10,8 @@ assign controls =
 (Opcode == 7'b0110011) ? 11'b0_00_xx_1_0_0_0_10: // r type
 (Opcode == 7'b0000011) ? 11'b1_01_00_1_0_0_0_00: // lw type
 (Opcode == 7'b0100011) ? 11'b1_xx_01_0_1_0_0_00: // sw type
-(sw)
-(Opcode == 7'b1100011) ? 11'b0_xx_10_0_0_1_0_01: // sb
-type(beq)
+(Opcode == 7'b1100011) ? 11'b0_xx_10_0_0_1_0_01: // sb type
 (Opcode == 7'b0010011) ? 11'b1_00_00_1_0_0_0_10: // addi type
-(addi)
 (Opcode == 7'b1101111) ? 11'bx_10_11_1_0_0_1_xx: // j type
-(jal)
 11'b0_00_00_0_0_0_0_00;
 endmodule
